fae4484ae44de3e499052513eca120d76d2d6475
se4oev <p.karpenko26@gmail.com>
2023-10-22 13:53:07 +0300

    HW03 add scripts to create and populate meals table;

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -10,3 +10,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES ((SELECT u.id FROM users u WHERE u.name = 'User'), '2023-10-20 10:38:46', 'Завтрак', 500);
+       ((SELECT u.id FROM users u WHERE u.name = 'User'), '2023-10-20 13:38:51', 'Обед', 700),
+       ((SELECT u.id FROM users u WHERE u.name = 'User'), '2023-10-20 18:00:01', 'Ужин', 400),
+       ((SELECT u.id FROM users u WHERE u.name = 'Admin'), '2023-10-20 09:03:05', 'Завтра', 400),
+       ((SELECT u.id FROM users u WHERE u.name = 'Admin'), '2023-10-20 13:31:00', 'Обед', 300),
+       ((SELECT u.id FROM users u WHERE u.name = 'Admin'), '2023-10-20 20:16:01', 'Хрючево', 1200);
